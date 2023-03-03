8ecc1fc85a76fe9db9ee49b8cd705dd200cc3158
RaquelWriter <raquel@mundofeliz.com>
2023-03-03 18:39:02 +0100

    feat: add the iframe for the map

--- geolocalization/script.js
+++ geolocalization/script.js
@@ -4,9 +4,23 @@ const map = document.getElementById('map');
 
 // Geolocalization API
 
-// Check if we have in navigator the property geolocation
-if ('geolocation' in navigator) {
-  navigator.geolocation.getCurrentPosition((position) => {
-    console.log(position.coords.latitude, position.coords.longitude);
-  });
-}
+const getMyLocation = () => {
+  // Check if we have in navigator the property geolocation
+  if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const lat = position.coords.latitude;
+      const long = position.coords.longitude;
+      map.innerHTML = `<iframe
+        src="https://maps.google.com/maps?q=${lat},${long}&z=15&output=embed"
+        width="100%"
+        height="100%"
+        frameborder="0"
+        style="border: 0"
+        allowfullscreen
+      ></iframe
+      >;`;
+    });
+  }
+};
+
+btn.addEventListener('click', getMyLocation);
